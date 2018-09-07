107ba7a9454397cccfd0d2e6471e8468375dcefe
Ryan Eisenbarth <ryan.eisenbarth@gmail.com>
2018-09-07 15:09:54 -0400

    Roman Map

--- main.cpp
+++ main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
+#include <map>
+/*
+ * Converts roman numeral to integer number
+ */
 
-#include "coin.h""
-
+using namespace std;
 int main() {
+    map<char, int> romanMap;
+    romanMap['I'] = 1;
+    romanMap['V'] = 5;
+    romanMap['X'] = 10;
+    romanMap['L'] = 50;
+    romanMap['C'] = 100;
+    romanMap['D'] = 500;
+    romanMap['M'] = 1000;
 
-    Coin c;
+    romanMap['i'] = 1;
+    romanMap['v'] = 5;
+    romanMap['x'] = 10;
+    romanMap['l'] = 50;
+    romanMap['c'] = 100;
+    romanMap['d'] = 500;
+    romanMap['m'] = 1000;
 
-    Coin *cptr;
-
-    cptr = c.flipped();
-
-    std::cout << "Hello, World!" << std::endl;
-
-    delete cptr;
+    int prevVal = 0;
+    int total = 0;
+    string romanInput;
+    cout << "Enter a Roman Numeral: ";
+    while(cin >> romanInput) {
+        for( auto it = romanInput.rbegin(); it != romanInput.rend(); it++) {
+            int curVal = romanMap[*it];
+            if(curVal >= prevVal)
+                total += curVal;
+            else
+                total -= curVal;
+            prevVal = curVal;
+        }
+        cout << "Value is " << total << endl;
+        cout << "Enter a roman numeral: ";
+    }
     return 0;
 }
